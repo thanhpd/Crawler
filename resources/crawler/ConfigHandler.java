afce143257c5b92eb372962e8aaed8caa9c94178
thanhpd <thanhpd.mac@gmail.com>
2015-11-25 02:16:55 +0700

    update

--- resources/crawler/ConfigHandler.java
+++ resources/crawler/ConfigHandler.java
@@ -33,115 +33,115 @@ public class ConfigHandler {
 	}
 	
 	private static void createConfigFile() {
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-			// root elements
-			Document doc = docBuilder.newDocument();
-			Element rootElement = doc.createElement("configuration");
-			doc.appendChild(rootElement);
-			
-			// server host element
-			Element serverHost = doc.createElement("serverHost");
-			serverHost.appendChild(doc.createTextNode("localhost"));
-			rootElement.appendChild(serverHost);
-			
-			// port element
-			Element port = doc.createElement("port");
-			port.appendChild(doc.createTextNode("8983"));
-			rootElement.appendChild(port);
-			
-			// core element
-			Element core = doc.createElement("core");
-			core.appendChild(doc.createTextNode("new_core"));
-			rootElement.appendChild(core);
-			
-			// solr path
-			Element solrPath = doc.createElement("solrPath");
-			solrPath.appendChild(doc.createTextNode("solr"));
-			rootElement.appendChild(solrPath);
-			
-			// storage folder
-			Element crawlStorageFolder = doc.createElement("crawlStorageFolder");
-			crawlStorageFolder.appendChild(doc.createTextNode("/data/crawl/root"));
-			rootElement.appendChild(crawlStorageFolder);
-			
-			// number of crawlers
-			Element numberOfCrawler = doc.createElement("numberOfCrawler");
-			numberOfCrawler.appendChild(doc.createTextNode("3"));
-			rootElement.appendChild(numberOfCrawler);
-			
-			Element crawlers = doc.createElement("crawlers");
-			rootElement.appendChild(crawlers);
-			
-			// first crawler
-			Element crawler_1 = doc.createElement("crawler");
-			crawlers.appendChild(crawler_1);
-			
-			Element maxPagesToFetch_1 = doc.createElement("maxPagesToFetch");
-			maxPagesToFetch_1.appendChild(doc.createTextNode("1000"));
-			
-			Element individualFolder_1 = doc.createElement("individualFolder");
-			individualFolder_1.appendChild(doc.createTextNode("crawler1"));
-			
-			Element delay_1 = doc.createElement("delay");
-			delay_1.appendChild(doc.createTextNode("1000"));
-			
-			Element crawlersDomain_1 = doc.createElement("crawlersDomain");					
-			crawlersDomain_1.appendChild(doc.createElement("domain").appendChild(doc.createTextNode("http:///www.ics.uci.edu/")));
-			crawlersDomain_1.appendChild(doc.createElement("domain").appendChild(doc.createTextNode("http://www.cnn.com/")));
-			
-			Element concurrentThreads_1 = doc.createElement("concurrentThreads");
-			concurrentThreads_1.appendChild(doc.createTextNode("5"));
-			
-			Element domainSeeders_1 = doc.createElement("domainSeeders"); 
-			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http:///www.ics.uci.edu/")));
-			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.cnn.com/")));
-			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.ics.uci.edu/~lopes/")));
-			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.cnn.com/POLITICS/")));
-			
-			Element maxDepth_1 = doc.createElement("maxDepth");
-			maxDepth_1.appendChild(doc.createTextNode("-1"));
-			
-			Element includeBinary_1 = doc.createElement("includeBinary");
-			includeBinary_1.appendChild(doc.createTextNode("1"));
-			
-			Element maxTimeoutSocket_1 = doc.createElement("maxTimeoutSocket");
-			maxTimeoutSocket_1.appendChild(doc.createTextNode("10000"));
-			
-			Element maxTimeoutConnection_1 = doc.createElement("maxTimeoutConnection");
-			maxTimeoutConnection_1.appendChild(doc.createTextNode("10000"));
-			
-			crawler_1.appendChild(maxPagesToFetch_1);
-			crawler_1.appendChild(maxDepth_1);
-			crawler_1.appendChild(maxTimeoutConnection_1);
-			crawler_1.appendChild(maxTimeoutSocket_1);
-			crawler_1.appendChild(includeBinary_1);
-			crawler_1.appendChild(individualFolder_1);
-			crawler_1.appendChild(domainSeeders_1);
-			crawler_1.appendChild(concurrentThreads_1);
-			crawler_1.appendChild(delay_1);
-			crawler_1.appendChild(crawlersDomain_1);
-			
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("file.xml"));
-
-			// Output to console for testing
-			// StreamResult result = new StreamResult(System.out);
-
-			transformer.transform(source, result);
-
-			System.out.println("File saved!");
-
-		  } catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		  } catch (TransformerException tfe) {
-			tfe.printStackTrace();
-		  }		
+//		try {
+//			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+//			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+//
+//			// root elements
+//			Document doc = docBuilder.newDocument();
+//			Element rootElement = doc.createElement("configuration");
+//			doc.appendChild(rootElement);
+//			
+//			// server host element
+//			Element serverHost = doc.createElement("serverHost");
+//			serverHost.appendChild(doc.createTextNode("localhost"));
+//			rootElement.appendChild(serverHost);
+//			
+//			// port element
+//			Element port = doc.createElement("port");
+//			port.appendChild(doc.createTextNode("8983"));
+//			rootElement.appendChild(port);
+//			
+//			// core element
+//			Element core = doc.createElement("core");
+//			core.appendChild(doc.createTextNode("new_core"));
+//			rootElement.appendChild(core);
+//			
+//			// solr path
+//			Element solrPath = doc.createElement("solrPath");
+//			solrPath.appendChild(doc.createTextNode("solr"));
+//			rootElement.appendChild(solrPath);
+//			
+//			// storage folder
+//			Element crawlStorageFolder = doc.createElement("crawlStorageFolder");
+//			crawlStorageFolder.appendChild(doc.createTextNode("/data/crawl/root"));
+//			rootElement.appendChild(crawlStorageFolder);
+//			
+//			// number of crawlers
+//			Element numberOfCrawler = doc.createElement("numberOfCrawler");
+//			numberOfCrawler.appendChild(doc.createTextNode("3"));
+//			rootElement.appendChild(numberOfCrawler);
+//			
+//			Element crawlers = doc.createElement("crawlers");
+//			rootElement.appendChild(crawlers);
+//			
+//			// first crawler
+//			Element crawler_1 = doc.createElement("crawler");
+//			crawlers.appendChild(crawler_1);
+//			
+//			Element maxPagesToFetch_1 = doc.createElement("maxPagesToFetch");
+//			maxPagesToFetch_1.appendChild(doc.createTextNode("1000"));
+//			
+//			Element individualFolder_1 = doc.createElement("individualFolder");
+//			individualFolder_1.appendChild(doc.createTextNode("crawler1"));
+//			
+//			Element delay_1 = doc.createElement("delay");
+//			delay_1.appendChild(doc.createTextNode("1000"));
+//			
+//			Element crawlersDomain_1 = doc.createElement("crawlersDomain");					
+//			crawlersDomain_1.appendChild(doc.createElement("domain").appendChild(doc.createTextNode("http:///www.ics.uci.edu/")));
+//			crawlersDomain_1.appendChild(doc.createElement("domain").appendChild(doc.createTextNode("http://www.cnn.com/")));
+//			
+//			Element concurrentThreads_1 = doc.createElement("concurrentThreads");
+//			concurrentThreads_1.appendChild(doc.createTextNode("5"));
+//			
+//			Element domainSeeders_1 = doc.createElement("domainSeeders"); 
+//			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http:///www.ics.uci.edu/")));
+//			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.cnn.com/")));
+//			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.ics.uci.edu/~lopes/")));
+//			domainSeeders_1.appendChild(doc.createElement("seeder").appendChild(doc.createTextNode("http://www.cnn.com/POLITICS/")));
+//			
+//			Element maxDepth_1 = doc.createElement("maxDepth");
+//			maxDepth_1.appendChild(doc.createTextNode("-1"));
+//			
+//			Element includeBinary_1 = doc.createElement("includeBinary");
+//			includeBinary_1.appendChild(doc.createTextNode("1"));
+//			
+//			Element maxTimeoutSocket_1 = doc.createElement("maxTimeoutSocket");
+//			maxTimeoutSocket_1.appendChild(doc.createTextNode("10000"));
+//			
+//			Element maxTimeoutConnection_1 = doc.createElement("maxTimeoutConnection");
+//			maxTimeoutConnection_1.appendChild(doc.createTextNode("10000"));
+//			
+//			crawler_1.appendChild(maxPagesToFetch_1);
+//			crawler_1.appendChild(maxDepth_1);
+//			crawler_1.appendChild(maxTimeoutConnection_1);
+//			crawler_1.appendChild(maxTimeoutSocket_1);
+//			crawler_1.appendChild(includeBinary_1);
+//			crawler_1.appendChild(individualFolder_1);
+//			crawler_1.appendChild(domainSeeders_1);
+//			crawler_1.appendChild(concurrentThreads_1);
+//			crawler_1.appendChild(delay_1);
+//			crawler_1.appendChild(crawlersDomain_1);
+//			
+//			// write the content into xml file
+//			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+//			Transformer transformer = transformerFactory.newTransformer();
+//			DOMSource source = new DOMSource(doc);
+//			StreamResult result = new StreamResult(new File("file.xml"));
+//
+//			// Output to console for testing
+//			// StreamResult result = new StreamResult(System.out);
+//
+//			transformer.transform(source, result);
+//
+//			System.out.println("File saved!");
+//
+//		  } catch (ParserConfigurationException pce) {
+//			pce.printStackTrace();
+//		  } catch (TransformerException tfe) {
+//			tfe.printStackTrace();
+//		  }		
 //		Properties prop = new Properties();
 //		OutputStream output = null;
 //
